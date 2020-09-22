af7958536f62c50c240958c1ba858f4f5fcfd41d
Lyuben Todorov <lyuben.todorov@gmail.com>
2020-09-22 19:11:49 +0300

    Update piko-cli

--- node/src/client.rs
+++ node/src/client.rs
@@ -15,7 +15,7 @@ use crate::proto::{ResourceRequest, ResourceRelease, MessageWrapper};
 use crate::req::publish::publish;
 use crate::state::State;
 
-use log::{info, error};
+use log::{info, error, debug};
 
 struct Client<'a> {
     identity: u64,
@@ -26,7 +26,7 @@ struct Client<'a> {
 pub enum ClientRes {
     Success {
         message: String,
-        bytes: Vec<u8>
+        bytes: Vec<u8>,
     },
     Error {
         message: String
@@ -42,8 +42,9 @@ enum ReqType {
     Publish,
 }
 
+
 #[derive(Serialize, Deserialize)]
-enum ReqBody {
+pub enum ClientReq {
     Poll {
         client_id: u64
     },
@@ -62,17 +63,21 @@ enum ReqBody {
     },
 }
 
-#[derive(Serialize, Deserialize)]
-pub struct ClientReq {
-    req_type: ReqType,
-    req_body: ReqBody,
-}
-
 impl ClientReq {
     pub fn sub(client_id: u64) -> ClientReq {
-        ClientReq {
-            req_type: ReqType::Subscribe,
-            req_body: ReqBody::Subscribe { client_id },
+        ClientReq::Subscribe {
+            client_id
+        }
+    }
+    pub fn unsub(client_id: u64) -> ClientReq {
+        ClientReq::Unsubscribe {
+            client_id
+        }
+    }
+    pub fn publ(client_id: u64, message: Vec<u8>) -> ClientReq {
+        ClientReq::Publish {
+            client_id,
+            message,
         }
     }
 }
@@ -98,11 +103,23 @@ fn read_req(stream: &mut TcpStream) -> Result<ClientReq, Box<dyn Error>> {
 /// Write Response to client
 ///
 fn write_res(stream: &mut TcpStream, res: ClientRes) {
-
     let buf = serde_cbor::to_vec(&res).unwrap();
 
-    stream.write_u8(buf.len() as u8);
+    debug!("Writing {} bytes to client", buf.len());
+    stream.write_u8(buf.len() as u8).unwrap();
     stream.write_all(buf.as_slice()).unwrap();
+}
+
+fn ok(stream: &mut TcpStream) {
+    write_res(stream, ClientRes::Success { message: "Ok".to_string(), bytes: vec![] });
+}
+
+fn ok_with_message(stream: &mut TcpStream, message: &str) {
+    write_res(stream, ClientRes::Success { message: message.to_string(), bytes: vec![] });
+}
+
+fn err(stream: &mut TcpStream, message: &str) {
+    write_res(stream, ClientRes::Error { message: message.to_string() });
 }
 
 pub fn client_listener(listener: TcpListener, state: Arc<RwLock<State>>, pledge_queue: Arc<Mutex<BinaryHeap<ResourceRequest>>>, _f_access: Arc<Mutex<bool>>) {
@@ -116,88 +133,93 @@ pub fn client_listener(listener: TcpListener, state: Arc<RwLock<State>>, pledge_
         let pledge_queue = Arc::clone(&pledge_queue);
 
         rayon::spawn(move || {
+            // debug!("Received message from client!");
             let req = match read_req(&mut stream) {
                 Ok(req) => req,
                 Err(e) => {
+                    error!("Failed reading message from client! {}", e);
                     return;
                 }
             };
 
-            match req.req_type {
-                ReqType::Subscribe => {
-                    if let ReqBody::Subscribe { client_id } = req.req_body {
-                        let client: Client = Client {
-                            identity: client_id,
-                            message_queue: VecDeque::<&Vec<u8>>::new(),
-                        };
+            match req {
+                ClientReq::Subscribe { client_id } => {
+                    debug!("Sub request from client {}", client_id);
+                    let client: Client = Client {
+                        identity: client_id,
+                        message_queue: VecDeque::<&Vec<u8>>::new(),
+                    };
 
-                        client_list.write().unwrap().insert(client_id, RwLock::from(client));
+                    let write = client_list.write().unwrap().insert(client_id, RwLock::from(client));
+                    match write {
+                        None => ok(&mut stream),
+                        Some(_) => ok_with_message(&mut stream, "Client exists, flushed queue")
                     }
                 }
-                ReqType::Unsubscribe => {
-                    if let ReqBody::Unsubscribe { client_id } = req.req_body {
-                        client_list.write().unwrap().remove(&client_id);
+                ClientReq::Unsubscribe { client_id } => {
+                    debug!("Unsub request from client {}", client_id);
+
+                    let v = client_list.write().unwrap().remove(&client_id);
+                    match v {
+                        None => err(&mut stream, "Client wasn't previously subscribed"),
+                        Some(_) => ok(&mut stream)
                     }
                 }
-                ReqType::Poll => {
-                    if let ReqBody::Poll { client_id } = req.req_body {
-                        let client_list = client_list.read().unwrap();
-                        let mut message_dequeue = client_list.get(&client_id).unwrap().write().unwrap();
-                        let message = message_dequeue.message_queue.pop_front();
-                        match message {
-                            None => {
-                                // write empty buffer
-                                let res= ClientRes::Success {
-                                    message: "".to_string(),
-                                    bytes: vec![]
-                                };
-                                write_res(&mut stream, res);
-                            }
-                            Some(message) => {
-                                let res = ClientRes::Success {
-                                    message: "".to_string(),
-                                    bytes: message.to_vec()
-                                };
-                                write_res(&mut stream, res)
-                            }
+                ClientReq::Poll { client_id } => {
+                    let client_list = client_list.read().unwrap();
+                    let mut message_dequeue = client_list.get(&client_id).unwrap().write().unwrap();
+                    let message = message_dequeue.message_queue.pop_front();
+                    match message {
+                        None => {
+                            // write empty buffer
+                            let res = ClientRes::Success {
+                                message: "".to_string(),
+                                bytes: vec![],
+                            };
+                            write_res(&mut stream, res);
+                        }
+                        Some(message) => {
+                            let res = ClientRes::Success {
+                                message: "".to_string(),
+                                bytes: message.to_vec(),
+                            };
+                            write_res(&mut stream, res)
                         }
                     }
                 }
-                ReqType::LongPoll => {}
-                ReqType::Publish => {
-                    if let ReqBody::Publish { client_id, message } = req.req_body {
-                        info!("Publishing message from client {} with size {}", client_id, message.len());
+                ClientReq::LongPoll { client_id } => {}
+                ClientReq::Publish { client_id, message } => {
+                    info!("Publishing message from client {} with size {}", client_id, message.len());
 
-                        let state_ref = state_ref.write().unwrap();
+                    let state_ref = state_ref.write().unwrap();
 
-                        let message_hash: [u8; 32] = Sha256::digest(message.as_slice()).into();
-                        let timestamp = Utc::now();
+                    let message_hash: [u8; 32] = Sha256::digest(message.as_slice()).into();
+                    let timestamp = Utc::now();
 
-                        let req: ResourceRequest = ResourceRequest {
-                            owner: *crate::proto::SENDER.lock().unwrap(),
-                            message_hash,
-                            timestamp,
+                    let req: ResourceRequest = ResourceRequest {
+                        owner: *crate::proto::SENDER.lock().unwrap(),
+                        message_hash,
+                        timestamp,
+                        sequence: 0,
+                    };
+
+                    let mut pledge_queue = pledge_queue.lock().unwrap();
+                    pledge_queue.push(req);
+                    drop(pledge_queue);
+
+                    publish(&state_ref.get_neighbour_addrs(), req);
+                    let _release: ResourceRelease = ResourceRelease {
+                        owner: *crate::proto::SENDER.lock().unwrap(),
+                        message_hash,
+                        timestamp,
+                        message: MessageWrapper {
+                            message,
                             sequence: 0,
-                        };
-
-                        let mut pledge_queue = pledge_queue.lock().unwrap();
-                        pledge_queue.push(req);
-                        drop(pledge_queue);
-
-                        publish(&state_ref.get_neighbour_addrs(), req);
-                        let _release: ResourceRelease = ResourceRelease {
-                            owner: *crate::proto::SENDER.lock().unwrap(),
-                            message_hash,
-                            timestamp,
-                            message: MessageWrapper {
-                                message,
-                                sequence: 0,
-                                receiver_mask: 0,
-                            },
-                            local: false,
-                            sequence: 0,
-                        };
-                    }
+                            receiver_mask: 0,
+                        },
+                        local: false,
+                        sequence: 0,
+                    };
                 }
             }
         })
